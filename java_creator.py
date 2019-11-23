c6c33992c565056b4bdba7c691eaa27d50781963
zhaoshengzhi <iloveyesterday@outlook.com>
2019-11-23 20:16:35 +0800

    add comments

--- java_creator.py
+++ java_creator.py
@@ -7,28 +7,59 @@ class SpringMvcCreator:
 
     @staticmethod
     def _get_template(template_file):
+        """
+        读取模板文件内容
+        :param template_file: 模板文件路径
+        :return: 模板文件内容
+        """
         with open(template_file, 'r', encoding='utf-8') as file:
             content = file.read()
         return content
 
     @staticmethod
     def _write_file(content, target_file):
+        """
+        将内容写入文件
+        :param content: 内容
+        :param target_file: 文件路径
+        :return:
+        """
         with open(target_file, 'w', encoding='utf-8') as file:
             file.write(content)
 
     def _write_default_file(self, template_file, target_file):
+        """
+        将模板文件的内容写入新文件
+        :param template_file: 模板文件路径
+        :param target_file: 新文件路径
+        :return:
+        """
         content = self._get_template(template_file)
         self._write_file(content, target_file)
 
     def _create_file_with_replace(self, file_dir, file, base_package):
+        """
+        生成文件, 并将包名写入
+        :param file_dir: 文件所在的文件夹
+        :param file: 文件名
+        :param base_package: 包
+        :return:
+        """
         if not os.path.exists(file_dir):
             os.makedirs(file_dir)
         f = os.path.join(file_dir, file)
         content = self._get_template('templates/spring-mvc/' + file)
+        # 替换包名
         content = re.sub('%BASE_PACKAGE', base_package, content)
         self._write_file(content, f)
 
     def _create_java(self, java_dir, base_package):
+        """
+        生成java源代码
+        :param java_dir: java文件夹
+        :param base_package: 包
+        :return:
+        """
         packages = base_package.split('.')
         for package in packages:
             java_dir = os.path.join(java_dir, package)
@@ -55,6 +86,12 @@ class SpringMvcCreator:
         self._create_file_with_replace(interceptor_package, 'AuthorityInterceptor.java', base_package)
 
     def _create_resources(self, resources_dir, base_package):
+        """
+        生成资源文件夹
+        :param resources_dir: 资源文件夹
+        :param base_package: 包
+        :return:
+        """
         os.makedirs(resources_dir)
         # app.properties
         f = os.path.join(resources_dir, 'app.properties')
@@ -75,6 +112,12 @@ class SpringMvcCreator:
         self._create_file_with_replace(mappers_dir, 'TestMapper.xml', base_package)
 
     def _create_webapp(self, webapp_dir, base_package):
+        """
+        生成webapp
+        :param webapp_dir: webapp文件夹
+        :param base_package: 包
+        :return:
+        """
         os.makedirs(webapp_dir)
         # index.jsp
         f = os.path.join(webapp_dir, 'index.jsp')
@@ -85,10 +128,23 @@ class SpringMvcCreator:
 
     @staticmethod
     def _create_test(test_dir):
+        """
+        生成test文件夹
+        :param test_dir: test文件夹
+        :return:
+        """
         os.makedirs(os.path.join(test_dir, 'java'))
         os.makedirs(os.path.join(test_dir, 'resources'))
 
     def _create_pom(self, project_dir, group_id, artifact_id, version):
+        """
+        生成pom.xml文件
+        :param project_dir: 项目文件夹
+        :param group_id: Group Id
+        :param artifact_id: Artifact Id
+        :param version: 版本号
+        :return:
+        """
         if not os.path.exists(project_dir):
             os.makedirs(project_dir)
         # pom.xml
@@ -101,6 +157,11 @@ class SpringMvcCreator:
         self._write_file(content, f)
 
     def _create_git_ignore(self, project_dir):
+        """
+        生成.gitignore文件
+        :param project_dir: 项目文件夹
+        :return:
+        """
         if not os.path.exists(project_dir):
             os.makedirs(project_dir)
         # .gitignore
